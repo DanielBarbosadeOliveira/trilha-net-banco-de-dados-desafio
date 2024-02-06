e1138aadfc217c059cedcb95f8b216da853ee094
danielb <dan.dam@hotmail.com>
2024-02-06 06:54:31 -0400

    Inserido script do desafio concluído

---
+++ Desafio Concluido.sql
@@ -0,0 +1,100 @@
+--1
+SELECT 
+	Nome,
+	Ano
+FROM Filmes
+
+--2
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+ORDER By Ano
+
+--3
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro'
+
+--4
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Ano = 1997
+
+--5
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Ano > 2000
+
+--6
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER By Duracao
+
+--7
+SELECT 
+	Ano,
+	COUNT(Nome) Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+--8
+SELECT
+	Id,
+	PrimeiroNome,
+	UltimoNome,
+	Genero
+FROM Atores
+WHERE Genero = 'M'
+
+--9
+SELECT
+	Id,
+	PrimeiroNome,
+	UltimoNome,
+	Genero
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+--10
+SELECT 
+	Filmes.Nome,	
+	Generos.Genero
+From Filmes
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+
+--11
+SELECT 
+	Filmes.Nome,	
+	Generos.Genero
+From Filmes
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+WHERE Genero = 'Mistério'
+
+--12
+SELECT 
+	Filmes.Nome,
+	Atores.PrimeiroNome,
+	Atores.UltimoNome,
+	ElencoFilme.Papel
+From Filmes
+INNER JOIN ElencoFilme ON Filmes.Id = ElencoFilme.IdFilme
+INNER JOIN Atores ON Atores.Id = ElencoFilme.IdAtor
